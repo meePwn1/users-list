a8c869718e753e42a9e567be58575ca9b46f21d8
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-07-10 23:48:14 +0500

    fix: настройка общих стилей

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import { StrictMode } from 'react'
- 
 import { createRoot } from 'react-dom/client'
- 
+
+import './styles/index.scss'
+
 import { App } from './App'
- 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+
+createRoot(document.getElementById('root') as HTMLElement).render(<App />)
