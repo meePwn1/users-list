56925424415690f887d4a0fef6af2732f9db7425
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-07-17 14:42:06 +0500

    fix backButton

--- src/common/utils/users.utils.ts
+++
@@ -1,9 +0,0 @@
-export const getFavoriteUserDataFromLocalStorage = (): Record<string, '' | 'favorite'> => {
-  const data = localStorage.getItem('user')
-
-  return JSON.parse(data || '{}')
-}
-
-export const setFavoriteUserDataToLocalStorage = (data: Record<string, '' | 'favorite'>) => {
-  localStorage.setItem('user', JSON.stringify(data))
-}
